68cf6fe48eceb4feeb78c1c3a9e0f2c623f34bc8
Pannapat <pannapatsangkhapitak@gmail.com>
2026-04-21 09:46:44 +0700

    fix: add CORS and clean routing for simulate

--- planner_service/main.py
+++ planner_service/main.py
@@ -6,6 +6,15 @@ from fastapi import FastAPI
 app = FastAPI()
 cred_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ตอนนี้เปิดก่อน
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if cred_json:
     cred_dict = json.loads(cred_json)
@@ -21,10 +30,12 @@ from planner_service.app.routes import user_routes
 from planner_service.app.routes import config_routes
 
 # include router
-app.include_router(ai_routes.router)
 app.include_router(ai_routes.router, prefix="/ai")
 app.include_router(user_routes.router)
 app.include_router(commit_routes.router)
+app.include_router(config_routes.router, prefix="/config")
+
+
 
 @app.get("/")
 def root():
