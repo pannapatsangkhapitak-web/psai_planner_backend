9e3d4b2d2ded80c8d0a05e3b843464c792977126
Pannapat <pannapatsangkhapitak@gmail.com>
2026-04-21 14:00:13 +0700

    fix: pay to sub task

--- planner_service/app/mapper.py
+++ planner_service/app/mapper.py
@@ -3,8 +3,20 @@
 def payload_to_task(req):
     return req  # placeholder
 
-def payload_to_subtasks(req):
-    return []  # placeholder
+def payload_to_subtasks(task, durations_by_skill):
+    subtasks = []
+
+    for skill, days in durations_by_skill.items():
+        subtasks.append(
+            SubTask(
+                task_id=task.task_id,
+                skill=Skill[skill],
+                duration=days
+            )
+        )
+
+    return subtasks
+
 from planner_v2.core.models import Task
 from planner_v2.core.enums import WorkType, Skill
 
