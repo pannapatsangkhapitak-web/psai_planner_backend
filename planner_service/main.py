788ecff69bb31ead08e7cfa872f5fdeabc74f987
Pannapat <pannapatsangkhapitak@gmail.com>
2026-04-24 14:41:02 +0700

    fix config route prefix duplication add preflight2

--- planner_service/main.py
+++ planner_service/main.py
@@ -25,7 +25,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5509", "http://localhost:5509/"],  # หรือใส่ domain จริงก็ได้
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],  # 🔥 สำคัญ (ต้องมี OPTIONS)
     allow_headers=["*"],  # 🔥 สำคัญ (ต้อง allow Authorization)
 )
