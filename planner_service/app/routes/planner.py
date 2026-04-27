d3d085f1eb1bcc6f452cb768901c39b0116f4fe3
Pannapat <pannapatsangkhapitak@gmail.com>
2026-04-27 14:16:29 +0700

    fix: revise planner.py declare a router

--- planner_service/app/routes/planner.py
+++ planner_service/app/routes/planner.py
@@ -9,9 +9,9 @@ db = FirestoreDB().db
 @router.get("/{hotel_id}/archive")
 def get_archive(hotel_id: str):
 
-    logs = db.collection("task_history") \
-        .where("hotel_id", "==", hotel_id) \
-        .where("action", "==", "ARCHIVED") \
+    logs = db.collection("properties") \
+        .document(hotel_id) \
+        .collection("tasks_archive") \
         .stream()
 
     return {"items": [doc.to_dict() for doc in logs]}
